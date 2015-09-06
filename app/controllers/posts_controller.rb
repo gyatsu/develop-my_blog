73501443fb1b78ff9266512829b7dabc1e14315c
MR.Y <programming@yatsukouji.com>
2015-09-06 09:56:09 +0900

    before_actionを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def new
     @post = Post.new
   end
@@ -9,26 +10,28 @@ class PostsController < ApplicationController
     redirect_to "/posts/#{@post.id}"
   end
   def show
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
   def index
     @posts = Post.all
   end
   def edit
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
   def update
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     @post.update_attributes(post_params)
     redirect_to "/posts/#{@post.id}"
   end
   def destroy
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     @post.destroy
     redirect_to "/"
   end
 
 private
+  def set_post
+    @post = Post.find(params[:id])
   def post_params
     params.require(:post).permit(
       :title,
