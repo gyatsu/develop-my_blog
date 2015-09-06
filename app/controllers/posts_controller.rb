e9f5138d0f2320aa19c87e6fc1c1a6cb58b977cc
MR.Y <programming@yatsukouji.com>
2015-09-06 09:37:56 +0900

    destroyまで完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/"
+  end
 
 private
   def post_params
